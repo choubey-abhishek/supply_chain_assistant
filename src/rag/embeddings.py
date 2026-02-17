172e4589d46b2255f2272558046ba92c761678b8
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-02-17 21:17:44 +0530

    Create embeddings.py

---
+++ src/rag/embeddings.py
@@ -0,0 +1,8 @@
+from langchain_openai import OpenAIEmbeddings
+from config import OPENAI_API_KEY, EMBEDDING_MODEL
+
+def get_embeddings():
+    return OpenAIEmbeddings(
+        model=EMBEDDING_MODEL,
+        openai_api_key=OPENAI_API_KEY
+    )
