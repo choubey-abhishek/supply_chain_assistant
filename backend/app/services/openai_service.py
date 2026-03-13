f311b311942c6e23f2a46f9acb3fbbe60a52684e
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-03-13 19:30:28 +0530

    Create openai_service.py

---
+++ backend/app/services/openai_service.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+from app.utils.config import OPENAI_API_KEY
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def ask_ai(question: str):
+
+    prompt = f"""
+You are a professional Supply Chain Analyst.
+
+Provide clear insights about:
+- demand forecasting
+- inventory optimization
+- supplier risk
+- logistics efficiency
+
+Question:
+{question}
+"""
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role":"system","content":"You are a supply chain expert"},
+            {"role":"user","content":prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
