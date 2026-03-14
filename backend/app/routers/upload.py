f3afe060ddcee57efaa57e6a09283e29a397fd11
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-03-14 21:10:44 +0530

    Update upload.py

--- backend/app/routers/upload.py
+++ backend/app/routers/upload.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, UploadFile, File
-from app.services.csv_service import analyze_csv
+from backend.app.services.csv_service import analyze_csv
 
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_csv(file: UploadFile = File(...)):
-
+async def upload(file: UploadFile = File(...)):
     result = analyze_csv(file.file)
-
     return {"summary": result}
