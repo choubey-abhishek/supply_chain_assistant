c22b96ecbec470091727739920c1012413414cc1
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-03-17 10:10:57 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,7 @@ import axios from "axios"
 const API_URL = "https://supply-chain-assistant-4jy2.onrender.com/api"
 
 const api = axios.create({
-  baseURL: API_URL,
-  headers: { "Content-Type": "application/json" }
+  baseURL: API_URL
 })
 
 export default api
